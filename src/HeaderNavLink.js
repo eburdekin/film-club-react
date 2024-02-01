5afaf42e3340584c964b26c3cbfe721a2de6ab3f
Eileen <eileenburdekin@proton.me>
2024-01-31 17:47:46 -0800

    added active toggle for navlinks/profile links

--- src/HeaderNavLink.js
+++ src/HeaderNavLink.js
@@ -1,17 +1,17 @@
 const HeaderNavLink = ({ href, children }) => {
-  //   const pathname = usePathname();
-  //   const active = href === pathname;
+  const pathname = window.location.pathname;
+  const active = href === pathname;
 
   return (
     <a
       href={href}
-      className="hover:bg-gray-100 p-2 rounded block"
-      //   className={`hover:bg-gray-100 p-2 rounded block ${
-      //     active ||
-      //     (href.startsWith("/profile") && pathname.startsWith("/profile"))
-      //       ? "text-black font-semibold"
-      //       : "text-gray-500"
-      //   }`}
+      //   className="hover:bg-gray-100 p-2 rounded block"
+      className={`hover:bg-gray-100 dark:hover:text-black p-2 rounded block ${
+        active ||
+        (href.startsWith("/profile") && pathname.startsWith("/profile"))
+          ? "text-black font-semibold dark:text-white"
+          : "text-gray-500"
+      }`}
     >
       {children}
     </a>
--- src/ProfileNavLink.js
+++ src/ProfileNavLink.js
@@ -1,14 +1,14 @@
 const ProfileNavLink = ({ href, children }) => {
   //   const pathname = usePathname();
-  //   const active = href === pathname;
+  const active = href === window.location.pathname;
 
   return (
     <a
       href={href}
-      className="hover:bg-gray-100 p-2 rounded block text-sm"
-      //   className={`hover:bg-gray-100 p-2 rounded block text-sm ${
-      //     active ? "text-black font-semibold" : "text-gray-500"
-      //   }`}
+      //   className="hover:bg-gray-100 dark:text-white p-2 rounded block text-sm"
+      className={`hover:bg-gray-100 dark:hover:text-black p-2 rounded block text-sm ${
+        active ? "text-black dark:text-white font-semibold" : "text-gray-500"
+      }`}
     >
       {children}
     </a>
