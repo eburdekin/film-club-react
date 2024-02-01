15dd9947e252743bc6846f3445c8ee5dd9c50225
Eileen <eileenburdekin@proton.me>
2024-01-31 20:16:34 -0800

    make profile navigation responsive to screen size

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -2,13 +2,30 @@ import ProfileNavLink from "../components/ProfileNavLink";
 
 const ProfileLayout = ({ children }) => {
   return (
-    <div className="flex gap-8">
-      <aside className="flex-[2]">
+    <div className="flex flex-col md:flex-row gap-8">
+      {/* Navigation as tabs on small screens */}
+      <nav className="bg-gray-100 p-2 rounded-md md:hidden">
+        <ul className="flex justify-evenly">
+          <li>
+            <ProfileNavLink href="/profile/my_clubs">My Clubs</ProfileNavLink>
+          </li>
+          {/* Uncomment the following when needed */}
+          {/* <li>
+            <ProfileNavLink href="/profile/account">Account</ProfileNavLink>
+          </li> */}
+          <li>
+            <ProfileNavLink href="/profile/settings">Settings</ProfileNavLink>
+          </li>
+        </ul>
+      </nav>
+      {/* Sidebar navigation on larger screens */}
+      <aside className="hidden md:flex md:flex-[2]">
         <nav>
           <ul className="grid gap-3">
             <li>
               <ProfileNavLink href="/profile/my_clubs">My Clubs</ProfileNavLink>
             </li>
+            {/* Uncomment the following when needed */}
             {/* <li>
               <ProfileNavLink href="/profile/account">Account</ProfileNavLink>
             </li> */}
@@ -18,6 +35,7 @@ const ProfileLayout = ({ children }) => {
           </ul>
         </nav>
       </aside>
+      {/* Main content */}
       <div className="bg-gray-100 flex-[8] p-4 rounded min-h-[300px]">
         {children}
       </div>
