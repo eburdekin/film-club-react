6e3a5981e994543195e1f0091022877fca8e77cb
Eileen <eileenburdekin@proton.me>
2024-02-07 13:52:44 -0800

    add dark mode to loading screen

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -3,14 +3,14 @@ import React from "react";
 export default function LoadingScreen() {
   return (
     <div
-      className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50"
+      className="fixed top-0 left-0 w-full h-full dark:bg-gray-900 flex items-center justify-center z-50"
       style={{ zIndex: 9999 }}
     >
       {/* <div className="text-5xl"> */}
       <img
         src="/reel.png"
         alt="loading film screen"
-        className="animate-spin h-20 w-20 text-gray-400 dark:text-gray-600"
+        className="animate-spin h-20 w-20 dark:invert"
       />
       {/* </div> */}
     </div>
