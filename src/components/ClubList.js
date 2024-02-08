eff529a20ecb65095ea5c6537b9684758cbddbd3
Eileen <eileenburdekin@proton.me>
2024-02-07 19:05:25 -0800

    added my clubs to clubs page

--- src/components/ClubList.js
+++ src/components/ClubList.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 
+import { useUser } from "./UserContext";
+
 export default function ClubList() {
+  const { user } = useUser();
   const [clubs, setClubs] = useState([]);
 
   useEffect(() => {
@@ -22,8 +25,26 @@ export default function ClubList() {
 
   return (
     <div className="mt-8">
-      <span className="text-red-600">If logged in:</span>
-      <h3 className="text-2xl mb-6 dark:text-gray-300">My clubs</h3>
+      <div className="mb-8">
+        {user ? (
+          <h3 className="text-2xl mb-6 dark:text-gray-300">My clubs</h3>
+        ) : (
+          ""
+        )}
+        {user && user.clubs ? (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            {user.clubs.map((club) => (
+              <a href={`/clubs/${club.id}`} key={club.id}>
+                <div className="p-4 bg-gray-100 dark:bg-gray-300  rounded-md hover-effect">
+                  {club.name}
+                </div>
+              </a>
+            ))}
+          </div>
+        ) : (
+          <>Join some clubs!</>
+        )}
+      </div>
       <h3 className="text-2xl mb-6 dark:text-gray-300">All clubs</h3>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {clubs.map((club) => (
