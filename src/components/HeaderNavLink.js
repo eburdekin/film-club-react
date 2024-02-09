6c59618c9c05335c163539d5aa26dbd27914448d
Eileen <eileenburdekin@proton.me>
2024-02-09 15:10:39 -0800

    using activeclassname for header navlink

--- src/components/HeaderNavLink.js
+++ src/components/HeaderNavLink.js
@@ -1,19 +1,11 @@
 import { NavLink } from "react-router-dom";
 
 const HeaderNavLink = ({ href, children }) => {
-  const pathname = window.location.pathname;
-  const active = pathname.startsWith(href);
-
   return (
     <NavLink
       to={href}
-      //   className="hover:bg-gray-100 p-2 rounded block"
-      className={`hover:bg-gray-100 dark:hover:text-black p-2 rounded-xl block ${
-        active ||
-        (href.startsWith("/profile") && pathname.startsWith("/profile"))
-          ? "text-black font-bold dark:text-white"
-          : "text-gray-700 dark:text-gray-400"
-      }`}
+      className="hover:bg-gray-100 dark:hover:text-black p-2 rounded-xl block"
+      activeClassName="text-black font-bold dark:text-white"
     >
       {children}
     </NavLink>
