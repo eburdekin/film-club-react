b8275a2cab1a8b95d759bb7d7071ec11eb2ea4a8
Eileen <eileenburdekin@proton.me>
2024-02-13 16:34:52 -0800

    update margins

--- src/components/UI/H4.js
+++ src/components/UI/H4.js
@@ -1,3 +1,3 @@
 export default function H4({ children }) {
-  return <h4 className="text-xl mb-6 dark:text-gray-300">{children}</h4>;
+  return <h4 className="text-xl my-6 dark:text-gray-300">{children}</h4>;
 }
