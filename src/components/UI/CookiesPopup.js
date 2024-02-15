2f57be480b497988b366db1961a181d8c35eef04
Eileen <eileenburdekin@proton.me>
2024-02-14 18:33:56 -0800

    add styling to cookiespopup

--- src/components/UI/CookiesPopup.js
+++ src/components/UI/CookiesPopup.js
@@ -13,17 +13,24 @@ const CookiesPopup = ({ onAccept }) => {
   }
 
   return (
-    <div className="fixed bottom-0 left-0 w-full flex justify-center">
-      <div className="bg-white rounded-lg shadow-md p-4 mb-4">
-        <p className="text-sm">
-          This website uses cookies to improve user experience.
+    <div className="fixed bottom-4 left-0 w-full flex justify-center">
+      <div className="bg-gray-100 dark:bg-gray-800 rounded-lg border border-purple-300 dark:border-purple-500 p-3 mb-4">
+        <p className="text-sm dark:text-white">
+          FilmClub uses cookies to improve your experience.
         </p>
-        <button
-          onClick={handleAccept}
-          className=" bg-purple-500 dark:bg-purple-400 text-white dark:text-black dark:hover:text-white p-2 rounded-xl block hover-effect"
-        >
-          Accept Cookies
-        </button>
+        <div className="flex justify-center gap-4 mt-2">
+          <button
+            onClick={handleAccept}
+            className="block text-sm bg-purple-500 dark:bg-purple-400 text-white dark:text-black dark:hover:text-white p-2 rounded-xl w-1/2"
+          >
+            Accept Cookies
+          </button>
+          <span className="block text-sm border border-purple-300 dark:border-purple-500 dark:text-white dark:hover:text-white p-2 rounded-xl w-1/2 text-center">
+            <a href="https://secureprivacy.ai/blog/understanding-cookie-compliance">
+              Read More
+            </a>
+          </span>
+        </div>
       </div>
     </div>
   );
